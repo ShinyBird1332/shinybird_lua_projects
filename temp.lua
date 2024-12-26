7b8cee2f18e027ecaffa33f5bc9595ea769407e4
ShinyBird368 <NesterovD368@gmail.com>
2024-12-26 23:27:16 +0300

    deleted:    In_process_computers/турбины.lua 	modified:   temp.lua

--- temp.lua
+++ temp.lua
@@ -1,74 +1,20 @@
-local component = require("component")
-local me_interface = component.me_interface
+local comp = require("component")
+local robot_lib = require("robot")
 
--- Проверка наличия предмета в МЭ
-function check_item_in_me(item_name)
-    local items = me_interface.getItemsInNetwork()
-    for _, item in ipairs(items) do
-        if item.label == item_name and item.size > 0 then
-            print("\nПредмет найден в МЭ: " .. item_name .. " - " .. item.size .. " штук")
-            return true
+--первый этап - раскопка местности 3 на 3 чанка
+
+local START_POS_Y = 2 --79 --Тут важно указать высоту, на которой располагается робоот!
+local CHUNK = 2
+local CHUNK_COUNT = 3
+
+
+
+function main()
+    for i = 1, CHUNK * CHUNK_COUNT - 1 do
+        for J = 1, CHUNK * CHUNK_COUNT - 1 do
+            
         end
     end
-    return false
 end
 
--- Функция запуска автокрафта с проверкой
-function start_autocraft(item_name)
-    -- Сначала проверяем наличие предмета
-    if check_item_in_me(item_name) then
-        print("Крафт не требуется. Предмет уже есть: " .. item_name)
-        return true
-    end
-
-    -- Получаем список доступных крафтов
-    local craftables = me_interface.getCraftables()
-    if not craftables then
-        print("Ошибка: нет доступных шаблонов для крафта.")
-        return false
-    end
-
-    -- Ищем шаблон и запускаем крафт
-    for _, craftable in ipairs(craftables) do
-        local item = craftable.getItemStack()
-        if item.label == item_name then
-            print("Шаблон найден. Запуск автокрафта для: " .. item_name)
-            local request = craftable.request(1)
-
-            -- Проверка успешности запроса
-            if request.isCanceled() then
-                print("Ошибка: автокрафт отменён. Недостаточно ресурсов.")
-                return false
-            end
-
-            -- Ожидаем завершения крафта
-            print("Ожидание завершения крафта...")
-            while not request.isDone() do
-                os.sleep(1)
-                print("...")
-            end
-
-            print("Автокрафт завершён для: " .. item_name)
-            return true
-        end
-    end
-
-    print("Ошибка: не найден шаблон для: " .. item_name)
-    return false
-end
-
-local pressed_buttons = {
-    "Computer Case (Tier 3)", 
-    "Battery Upgrade (Tier 3)", 
-    "Crafting Upgrade",
-    "Inventory Controller Upgrade", 
-    "Trading Upgrade",
-    "Screen (Tier 1)", 
-    "Disk Drive", 
-    "Keyboard", 
-    "EEPROM (Lua BIOS)"
-}
-
-for _, item in ipairs(pressed_buttons) do
-    start_autocraft(item)
-end
+main()
