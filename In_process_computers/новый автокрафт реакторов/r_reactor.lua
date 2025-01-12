87d9fa2629d474850561f0422ce7a2b67c6d704a
ShinyBird368 <NesterovD368@gmail.com>
2025-01-13 00:24:26 +0300

    modified:   In_process_computers/новый автокрафт реакторов/main.lua 	modified:   In_process_computers/новый автокрафт реакторов/r_reactor.lua

--- In_process_computers/новый автокрафт реакторов/r_reactor.lua
+++ In_process_computers/новый автокрафт реакторов/r_reactor.lua
@@ -42,6 +42,22 @@ end
 function reactor.build_rods()
     --хз, вернется ли он на начало, потом доделаю тут
     --предположим, он уже на нужных кордах
+    for _ = 1, 7 do
+        for _ = 1, constants.SIZE - 2 do
+            for _ = 1, 30 do
+                functions.place_block("Yellorium Fuel Rod")
+                functions.repeat_swing("up")
+            end
+            for _ = 1, 30 do functions.repeat_swing("down") end
+            functions.repeat_swing("forward")
+        end
+    
+        constants.robot.turnAround()
+        functions.run(constants.SIZE - 2)
+        constants.robot.turnLeft()
+        functions.run(2)
+        constants.robot.turnLeft()
+    end --на этот моменте делаются только стержни 
     
 end
 
