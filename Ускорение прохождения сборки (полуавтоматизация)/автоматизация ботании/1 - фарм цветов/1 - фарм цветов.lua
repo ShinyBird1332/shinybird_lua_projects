d7695b93f9f770b52004aa51b0f1b0600be08b44
ShinyBird368 <NesterovD368@gmail.com>
2025-01-20 00:45:37 +0300

    modified:   Ускорение прохождения сборки (полуавтоматизация)/автоматизация ботании/1 - фарм цветов/1 - фарм цветов.lua

--- Ускорение прохождения сборки (полуавтоматизация)/автоматизация ботании/1 - фарм цветов/1 - фарм цветов.lua
+++ Ускорение прохождения сборки (полуавтоматизация)/автоматизация ботании/1 - фарм цветов/1 - фарм цветов.lua
@@ -2,20 +2,17 @@ local comp = require("component")
 local robot = require("robot")
 local sides = require("sides")
 local i_c = comp.inventory_controller
+local crafting = comp.crafting
+
+local flower_name = "Mystical"
 
 --начинаем с того, что справа есть 2 цветка и ступка, а сзади только кости
 
---вперед на 3
---заюзить удобрение########################################
---назад, 2 прямо, направо, 3 прямо, направо
---в цикле проходим 7 на 7
---назад, 3 прямо, налево, 1 прямо, разворот
 
---повернуться направо
---скинуть в сундук справа все цветы (надо придумать ортировку, чтоб они стакались все)
---(мб сначала забирать все цветы, потом складывать их орбратно)
+--повернуться направо --готово
+--скинуть в сундук справа все цветы (надо придумать ортировку, чтоб они стакались все) --готово
+--(мб сначала забирать все цветы, потом складывать их орбратно) -- готово
 
---начало алгоритма тут
 --взять с сундука первый стак в цветками (в 1-й слот)
 --скрафтить лепестков столько, сколько цветков (но не больше 64) (минимум 2 цветка)
 --взять с сундука справа ступку и положить во второй слот
@@ -30,10 +27,115 @@ local i_c = comp.inventory_controller
 --поместить её в слот инструмента#############################################
 --запустить цикл столько раз, сколько удобрений
 
+--вперед на 3
+--заюзить удобрение########################################
+--назад, 2 прямо, направо, 3 прямо, направо
+--в цикле проходим 7 на 7
+--назад, 3 прямо, налево, 1 прямо, разворот
+
+
+function grab_all_flowers()
+    for i = 1, i_c.getInventorySize(sides.front) do
+        local item = i_c.getStackInSlot(sides.front, i)
+
+        if item and item.label:find(flower_name) then
+            i_c.suckFromSlot(sides.front, i, _)
+        end
+    end
+end
+
+function suck_all_flowers()
+    for i = 1, robot.inventorySize() do
+        local item = i_c.getStackInInternalSlot(i)
+
+        if item and item.label:find(flower_name) then
+            robot.select(i)
+            robot.drop()
+        end
+    end
+    robot.select(1)
+end
+
+function grab_one_flower()
+    local count_flowers = 0
+    for i = 1, i_c.getInventorySize(sides.front) do
+        local item = i_c.getStackInSlot(sides.front, i)
+
+        if item and item.label:find(flower_name) and count_flowers < 2 then
+            i_c.suckFromSlot(sides.front, i, 1)
+            count_flowers = count_flowers + 1
+        end
+    end
+end
+
+function grab_one_stack_flower()
+    for i = 1, i_c.getInventorySize(sides.front) do
+        local item = i_c.getStackInSlot(sides.front, i)
+
+        if item and item.label:find(flower_name) then
+            
+            if item.size > 1 then
+                if item.size // 2 * 2 > 32 then c = 32 else c = item.size // 2 * 2 end
+                i_c.suckFromSlot(sides.front, i, c)
+                return "same"
+            else
+                grab_one_flower()
+                return "different"
+            end
+        end
+    end
+end
+
+function craft_dust(param)
+    if param == "same" then
+        crafting.craft()
+    elseif param == "different" then
+        robot.select(2)
+        robot.transferTo(4)
+        robot.select(8)
+        crafting.craft()
+        robot.select(4)
+        robot.transferTo(1)
+        crafting.craft()
+        robot.select(8)
+        robot.transferTo(2)
+        robot.select(1)
+
+        for i = 1, i_c.getInventorySize(sides.front) do
+            local item = i_c.getStackInSlot(sides.front, i)
+
+            if item and item.label:find("Pestle and Mortar") then
+                i_c.suckFromSlot(sides.front, i, 1)
+                break
+            end
+        end
+        robot.select(8)
+        crafting.craft()
+        robot.select(4)
+        robot.transferTo(2)
+        crafting.craft()
+        robot.select(1)
+        robot.drop()
+        robot.select(4)
+        robot.transferTo(2, 1)
+        robot.transferTo(3, 1)
+        robot.select(8)
+        robot.transferTo(5, 1)
+        robot.transferTo(6, 1)
+    end
+end
+
 function main()
+    robot.select(1)
+    robot.turnRight()
+    grab_all_flowers()
+    suck_all_flowers()
+    local param = grab_one_stack_flower()
+    craft_dust(param)
 
-    for _ = 1, 3 do robot.forward() end
 
+
+    robot.turnLeft()
 end
 
 main()
