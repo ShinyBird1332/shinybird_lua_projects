431315e161fa4aa633f2e2fbcac85e517b2d8e6a
ShinyBird368 <NesterovD368@gmail.com>
2025-01-25 00:39:08 +0300

    modified:   In_process_computers/автоизучение таума/comp.lua 	new file:   In_process_computers/автоизучение таума/farm_bones.lua 	modified:   In_process_computers/автоизучение таума/robot.lua

--- In_process_computers/автоизучение таума/comp.lua
+++ In_process_computers/автоизучение таума/comp.lua
@@ -1,52 +1,52 @@
---транспозер сканирует сундук на наличие свитков 
---(надо сделать так, чтоб он различал изученные и неизученные)
---если в слоте есть свиток, кидает его в выбрасыватель ботании (в сундук к выбрасывателю)
---свиток попадает на книгу, там пока работает печка и качает когнитио 
---(надо сделать простой способ фарма бумаги)
---после перемещения, с задержкой в секунду, адаптер начинает сканировать книгу
---в цикле, раз в 5 секунд, смотрим, сколько осталось каждого аспекта (не важны их названия)
---если все 3 по нулям, отдается команда роботу grab
-
---робот забирает свиток и передает его в итоговый сундук
---потом цикл продолжается на следующие слоты
+--Research Notes
+--Discovery
 
 local comp = require("component")
 local sides = require("sides")
-local event = require("event")
-
+local tunnel = comp.tunnel
 local trans = comp.transposer
-local book = comp.book --------------------------
+local book = comp.blockknowledgebook
 
 local side_chest_in = sides.south
 local side_chest_out = sides.west
 
 function scan_chest_in()
     for i = 1, trans.getInventorySize(side_chest_in) do
-        local slot = trans.getStackInSlot(i)
-        if slot and slot.label == "Название свитков" then --------------------------
+        local slot = trans.getStackInSlot(side_chest_in, i)
+        if slot and slot.label == "Research Notes" then
+            print("Начинаем изучение свитка...")
+            print("==========")
             trans.transferItem(side_chest_in, side_chest_out, 1, i, i)
             os.sleep(1)
             scan_book()
-            --тут надо дать команду роботу
+            tunnel.send("grab", 1)
         end
     end
 end
 
 function scan_book()
-    local stop = false
-    while not stop do
-        for _, value in pairs(book.function_for_scan_book1()) do --------------------------
-        local count = book.function_for_scan_book2(value) --------------------------
-            if count and count > 0 then 
-                stop = true
-            end
+    while true do
+        print("Осталось:")
+        local res = 0
+        for _, value in pairs(book.getAspects()) do 
+            
+            local count = book.getAspectCount(value) 
+            print(value .. ": " .. count)
+            res = res + count
+        end
+        print("==========")
+        os.sleep(5)
+        if res == 0 then 
+            print("++")
+            break
         end
     end
 end
 
 function main()
-    --это потом в цикл всунуть
-    scan_chest_in()
+    while true do
+        scan_chest_in()
+    end
 end
 
 main()
---
+++ In_process_computers/автоизучение таума/robot.lua
@@ -0,0 +1,28 @@
+local comp = require("component")
+local robot = require("robot")
+local event = require("event")
+
+local tractor_beam = comp.tractor_beam
+
+function grab_discovery()
+    tractor_beam.suck()
+    robot.turnAround()
+    for _ = 1, robot.inventorySize() do
+        if robot.count() > 0 then
+            robot.drop()
+        end
+    end
+    robot.turnAround()
+end
+
+function main()
+    while true do
+        local _, _, _, _, _, message, command = event.pull("modem_message")
+        if message == "grab" then
+            grab_discovery()
+        end
+        os.sleep(0.1)
+    end
+end
+
+main()
