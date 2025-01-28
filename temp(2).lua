5d266677647710fbee45fc6560707b450463a3ea
ShinyBird368 <NesterovD368@gmail.com>
2025-01-28 08:00:32 +0300

    modified:   In_process_computers/автоизучение таума/магаз.lua 	modified:   temp(2).lua

--- temp(2).lua
+++ temp(2).lua
@@ -5,20 +5,20 @@ local trans = comp.transposer
 
 local side = sides.north
 
-local item = trans.getStackInSlot(side, 1)
-print(item.name) -- ID предмета
-print(item.hasTag) -- Проверка, есть ли NBT
-print(item.tag)
+
 
 
 function t()
     for i = 1, trans.getInventorySize(side) do
         item = trans.getStackInSlot(side, i)
-        if item then
-            item = trans.getStackInSlot(side, i).label
-            print(item)
+        for k, v in pairs(item) do
+            print(k, v)
         end
+        print("+++++++++++++++++++")
     end
+    
 end
+
+t()
 
 -- Reactor Casing
