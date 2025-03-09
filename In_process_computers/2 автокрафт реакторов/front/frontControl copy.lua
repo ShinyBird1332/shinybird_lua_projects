c91353b83d6eb249491fb8a95e746500795b8d1c
ShinyBird368 <NesterovD368@gmail.com>
2025-03-09 23:40:30 +0300

    modified:   In_process_computers/2 автокрафт реакторов/front/frontControl copy.lua

--- In_process_computers/2 автокрафт реакторов/front/frontControl copy.lua
+++ In_process_computers/2 автокрафт реакторов/front/frontControl copy.lua
@@ -3,44 +3,44 @@ local frontControl = {}
 local constants = dofile("constants.lua")
 local frontAdditionalControl = dofile("frontAdditionalControl.lua")
 local guiModuls = dofile("guiModuls.lua")
+local colors = constants.colors
+local buttons = {}
 
-function frontControl.get_reactors_info()
-    constants.modem.broadcast(4, "wait_mes")
-    local _, _, _, _, _, message = constants.event.pull("modem_message")
-    -- Декодируем строку обратно в таблицу
-    local _, t = pcall(constants.serialization.unserialize, message)
-
-    for _, value in pairs(t) do
-        --print("Реактор №" .. value.reactor_number)
-        --print("Количество топлива: " .. value.reactor_count_fluid)
-        --print("Выработка энергии: " .. value.reactor_gen_energy_per_tick)
-        --print("Хранимая энергия: " .. value.reactor_energy_store)
-        --print("Состояние: " .. tostring(value.reactor_state))
-        --print("Потребление топлива: " .. value.reactor_consumed_fuel)
-        --print("Количество отходов: " .. value.reactor_waste)
-        --print("=======================================")
+function ttt()
+    for i = 1, 24 do
+        
     end
 end
 
 --отрисовка кнопок реакторов
 function frontControl.draw_reactors_buttons()
-    
+    local reactors = ttt()
+
+
+    local reactor_number = 1
     for i = 0, 4 do
         for j = 0, 4 do
-            local reactor_number = constants.reactors[1] or "N/A"
-            print(reactor_number)
+            local pos_x = j * cur_w
+            local pos_y = i * cur_h
+            local reactor_count_fluid = reactors[reactor_number].reactor_count_fluid or  "N/A"
+            local reactor_gen_energy = 5 or "N/A"
+
+            table.insert(
+                buttons, guiModuls.draw_button(
+                pos_x, pos_y, cur_w, cur_h, "", colors.gray, colors.black, colors.white, function() frontAdditionalControl.main(reactor_number) end)
+            )
+            constants.gpu.set((pos_x + cur_w / 2) - 7, pos_y + 3, "Реактор № " .. reactor_number .. "   ")
+ 
+            constants.gpu.set(pos_x + 8, pos_y + 5, "Кол-во топлива: " .. reactor_count_fluid .. "%")
+            constants.gpu.set(pos_x + 5, pos_y + 7, "Выработка энергии: " .. reactor_gen_energy .. " rf/t")
+            reactor_number = reactor_number + 1 
+
         end
     end
 end
 
 function frontControl.main()
-    frontControl.get_reactors_info()
     frontControl.draw_reactors_buttons()
-
-    while true do
-        local _, _, x, y = constants.event.pull("touch")
-        frontControl.handle_click(x, y)
-    end
 end
 
 frontControl.main()
